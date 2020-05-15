ce76e96ae30e9178a5578688ace0a4ec2fa389bd
Wagner Santos <Wagner_Santos@live.com>
2020-05-15 13:22:24 -0300

    Criando e implementando listar usuario por id

--- app/src/main/java/com/example/convidados/service/repository/PessoaRepository.kt
+++ app/src/main/java/com/example/convidados/service/repository/PessoaRepository.kt
@@ -73,6 +73,50 @@ class PessoaRepository private constructor(context: Context) {
     }
 
 
+    fun listarUsuarioId(id: Int): PessoaModel? {
+
+        var pessoa: PessoaModel? = null
+
+        return try {
+
+            val db = mDatabase.readableDatabase
+
+            var projecao = arrayOf(
+                DataBaseConstants.PESSOA.COLUNAS.NOME,
+                DataBaseConstants.PESSOA.COLUNAS.PRESENCA
+            )
+
+            val selecao = DataBaseConstants.PESSOA.COLUNAS.ID + " = ?"
+            val args = arrayOf(id.toString())
+
+            val cursor = db.query(
+                DataBaseConstants.PESSOA.TABLE_NAME,
+                projecao,
+                selecao,
+                args,
+                null,
+                null,
+                null
+            )
+
+            if (cursor != null && cursor.count > 0) {
+
+                cursor.moveToFirst()
+
+                val nome =
+                    cursor.getString(cursor.getColumnIndex(DataBaseConstants.PESSOA.COLUNAS.NOME))
+                val presenca =
+                    (cursor.getInt(cursor.getColumnIndex(DataBaseConstants.PESSOA.COLUNAS.PRESENCA)) == 1)
+
+                pessoa = PessoaModel(id, nome, presenca)
+            }
+            cursor?.close()
+            pessoa
+        } catch (e: Exception) {
+            pessoa
+        }
+    }
+
     fun listarTodasPessoa(): List<PessoaModel> {
         val list: MutableList<PessoaModel> = ArrayList()
         return list
